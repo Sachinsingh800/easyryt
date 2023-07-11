50aa77f5e467ff3d04ca79e9f04a46538c01e656
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-12 00:52:21 +0530

    css

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import style from "./NavBar.module.css";
-import logo from "../Image/logo.png";
+import logo from "../../Image/logo.png";
 
 function NavBar() {
   const [activeLink, setActiveLink] = useState(null);
