5843039ebcaa687c432f11902bda80d0325d73be
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-13 21:48:35 +0530

    css

--- src/Component/Slider/Slider.js
+++ src/Component/Slider/Slider.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import style from  "./Slider.module.css"
-import img from "../../Image/SAP_(6).png"
+import logo from "../../Image/Dominos.png"
+import logo1 from "../../Image/800px-Kentucky_Fried_Chicken_201x_logo.svg.png"
+import logo2 from "../../Image/Mars.png"
+import logo3 from "../../Image/pngwing.com (2).png"
+
  
 export default function Slider() {
   const responsive = {
@@ -30,28 +34,28 @@ export default function Slider() {
         <div>
           <img
             className={style.item}
-            src={img}
+            src={logo}
             alt="img 1"
           />
         </div>
         <div>
           <img
             className={style.item}
-            src={img}
+            src={logo1}
             alt="img 1"
           />
         </div>
         <div>
           <img
             className={style.item}
-            src={img}
+            src={logo2}
             alt="img 1"
           />
         </div>
         <div>
           <img
             className={style.item}
-            src={img}
+            src={logo3}
             alt="img 1"
           />
         </div>
--- src/Section/ForthSection/ForthSection.js
+++ src/Section/ForthSection/ForthSection.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Slider from '../../Component/Slider/Slider'
+import style from "./ForthSection.module.css"
 
 function ForthSection() {
   return (
-    <div>
+    <div className={style.main}>
+        <h1>Our clients</h1>
         <Slider/>
     </div>
   )
