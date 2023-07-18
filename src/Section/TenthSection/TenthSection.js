3939d1137270921963176e45942929f2e46ba230
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-18 15:23:22 +0530

    css

--- src/Section/TenthSection/TenthSection.js
+++ src/Section/TenthSection/TenthSection.js
@@ -13,49 +13,49 @@ const TenthSection = () => {
      no:1,
       image: logo1,
       heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      text: "Improves efficiency and provides high scalability, boosting productivity, cost-efficiency, and adaptability to changing market demands for businesses' success. ",
     },
     {
         no:2,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo2,
+      heading: "Designs,Wireframes & Mockups",
+      text: "Designs, wireframes, and mockups are essential elements in the development process, ensuring cohesive visual representation and user experience for web and app projects.",
     },
     {
         no:3,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo3,
+      heading: "Prototype Demo",
+      text: "A prototype demo is a visual representation of a product or software's functionalities and interactions, allowing stakeholders to preview its features before development completion.",
     },
     {
         no:4,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo4,
+      heading: "Change And Confirmation",
+      text: "Change and confirmation refer to the process of verifying modifications made to a product or system before implementing them, ensuring the accuracy and approval of the updates.",
     },
     {
         no:5,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo5,
+      heading: "SEO",
+      text: "SEO (Search Engine Optimization) is the process of improving a website's visibility on search engine results pages, increasing organic traffic and enhancing online presence for better business performance.",
     },
     {
         no:6,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo6,
+      heading: "Support And Maintenance",
+      text: "Support and maintenance encompass ongoing activities to ensure the smooth operation, updates, and resolution of issues for software, websites, or systems to keep them functional, secure, and up-to-date.",
     },
     {
         no:7,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo6,
+      heading: "Deployment",
+      text: "Deployment refers to the process of releasing and making software applications, websites, or systems available for public use or internal implementation after development and testing phases are complete.",
     },
     {
         no:8,
-      image: logo1,
-      heading: "Improves Efficiency And Offers High Scalability",
-      text: "Since Business Apps Are Custom Built With The Companys Needs In Mind They Act As A Robust App That Performs Various Tasks Eliminating The Need For Multiple Apps.",
+      image: logo6,
+      heading: "Development",
+      text: "Development is the process of creating and building software applications, websites, or systems by writing code, designing interfaces, and implementing functionalities to meet specific requirements and objectives..",
     },
     // Add more card data as needed...
   ];
@@ -72,7 +72,7 @@ const TenthSection = () => {
 
   return (
     <div className={style.main}>
-      <h1>Benefits Of Mobile App Development</h1>
+      <h1>Process We Follow</h1>
       <p>Here Are Some Of The Most Advantage Of Mobile App Growth For Business.</p>
       <div className={style.innerbox}>
         {cardsData.map((card, index) => (
