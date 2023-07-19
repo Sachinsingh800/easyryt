435519be228ca0868e27fe597ebe6f40c511b599
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-19 20:42:25 +0530

    css

--- src/Page/FirstPage/Section2/Section2.js
+++ src/Page/FirstPage/Section2/Section2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import style from "./Section2.module.css";
 import logo1 from "../../../Image/chart.gif";
 import logo2 from "../../../Image/date.gif";
@@ -7,7 +7,80 @@ import logo4 from "../../../Image/page.gif";
 import logo5 from "../../../Image/setting.gif";
 import logo6 from "../../../Image/message.gif";
 
+const cardData = [
+  {
+    logo: logo1,
+    title: "Android App",
+    description:
+      "Our Android applications are renowned for their captivating User Interface UI/UX, rapid loading speeds, and reliable performance.",
+  },
+  {
+    logo: logo2,
+    title: "iOS App",
+    description:
+      "Our iOS apps embody innovative concepts, featuring intuitive GUIs. With our proficient iPhone application development team, you'll find the perfect solution for your needs.",
+  },
+  {
+    logo: logo3,
+    title: "Tablet App development",
+    description:
+      "We offer extensive tablet app development solutions to diverse industries, catering to startups and leading enterprises alike.",
+  },
+  {
+    logo: logo4,
+    title: "iPad App Development",
+    description:
+      "With years of proficiency in native app development, our team can fulfill your unique requirements. We provide impeccable, visually appealing iPad app development services using Flutter.",
+  },
+  {
+    logo: logo5,
+    title: "AR App Development",
+    description:
+      "Benefiting from over 15 years of expertise in 3D modeling and Augmented Reality (AR), we deliver top-notch AR software swiftly. Our team crafts cutting-edge virtual reality solutions, harnessing the latest tools and advancements.",
+  },
+  {
+    logo: logo6,
+    title: "Marketing Automation Solutions",
+    description:
+      "Catering to diverse user needs, our team develops Native applications that precisely align with their business requirements. Leveraging the finest native app development platform, we ensure optimal results for every project.",
+  },
+];
+
+const lightColors = [
+  "#CAF1DE",
+  "#E1DBFF",
+  "#F0FFC9",
+  "#FFF3CB",
+  "#FFC6C6",
+  "#E0FF9F",
+]; // You can add more light colors here
+
+
+
 function Section2() {
+  const [randomColors, setRandomColors] = useState(lightColors);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRandomColors(getRandomColorsArray(cardData.length));
+    }, 10000); // 2-second delay
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const getRandomColorsArray = (length) => {
+    const colors = [];
+    for (let i = 0; i < length; i++) {
+      colors.push(getRandomColor());
+    }
+    return colors;
+  };
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * lightColors.length);
+    return lightColors[randomIndex];
+  };
+
   return (
     <div className={style.main}>
       <h1>The Expertise Of Our Native App Development Service</h1>
@@ -16,60 +89,13 @@ function Section2() {
         Native apps for different platforms. Take a look at our expertise:
       </p>
       <div className={style.innerbox}>
-        <div className={style.card}>
-          <img className={style.img} src={logo1} alt="img1" />
-          <h6>Android App</h6>
-          <p>
-            Our Android applications are renowned for their captivating User
-            Interface UI/UX, rapid loading speeds, and reliable performance.
-          </p>
-        </div>
-        <div className={style.card}>
-          <img className={style.img} src={logo2} alt="img1" />
-          <h6>IOS App</h6>
-          <p>
-            Our iOS apps embody innovative concepts, featuring intuitive GUIs.
-            With our proficient iPhone application development team, you'll find
-            the perfect solution for your needs.
-          </p>
-        </div>
-        <div className={style.card}>
-          <img className={style.img} src={logo3} alt="img1" />
-          <h6>Tablet App development</h6>
-          <p>
-            We offer extensive tablet app development solutions to diverse
-            industries, catering to startups and leading enterprises alike.
-          </p>
-        </div>
-        <div className={style.card}>
-          <img className={style.img} src={logo4} alt="img1" />
-          <h6>IPad App Development</h6>
-          <p>
-            With years of proficiency in native app development, our team can
-            fulfill your unique requirements. We provide impeccable, visually
-            appealing iPad app development services using Flutter.
-          </p>
-        </div>
-        <div className={style.card}>
-          <img className={style.img} src={logo5} alt="img1" />
-          <h6>AR App Development</h6>
-          <p>
-            Benefiting from over 15 years of expertise in 3D modeling and
-            Augmented Reality (AR), we deliver top-notch AR software swiftly.
-            Our team crafts cutting-edge virtual reality solutions, harnessing
-            the latest tools and advancements.
-          </p>
-        </div>
-        <div className={style.card}>
-          <img className={style.img} src={logo6} alt="img1" />
-          <h6>Marketing Automation Solutions</h6>
-          <p>
-            Catering to diverse user needs, our team develops Native
-            applications that precisely align with their business requirements.
-            Leveraging the finest native app development platform, we ensure
-            optimal results for every project.
-          </p>
-        </div>
+        {cardData.map((data, index) => (
+          <div className={style.card} key={index} style={{ background: randomColors[index] }}>
+            <img className={style.img} src={data.logo} alt={`img${index}`} />
+            <h6>{data.title}</h6>
+            <p>{data.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
