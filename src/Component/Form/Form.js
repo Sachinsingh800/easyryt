25dae1f6ad1caca6a44dc4ab947f5169eb899bc6
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-21 16:04:24 +0530

    mobile no field added

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import styles from './Form.module.css';
-import formImg from "../../Image/form.png"
+import formImg from '../../Image/form.png';
 import Swal from 'sweetalert2';
+import { countryCodes } from '../../Const/Const';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+
 
 const Form = () => {
   const [fullName, setFullName] = useState('');
+  const [countryCode, setCountryCode] = useState(countryCodes[0].code); // Default country code
   const [mobileNumber, setMobileNumber] = useState('');
   const [requestServices, setRequestServices] = useState('');
   const [email, setEmail] = useState('');
-
 
   const showAlert = () => {
     Swal.fire({
@@ -23,15 +28,37 @@ const Form = () => {
     // Perform form submission logic here
     console.log('Form submitted!');
     console.log('Full Name:', fullName);
-    console.log('Mobile Number:', mobileNumber);
+    console.log('Mobile Number:', countryCode + mobileNumber); // Combine country code and mobile number
     console.log('Request Services:', requestServices);
     console.log('Email:', email);
-    showAlert()
+    showAlert();
   };
+
+  const [filteredCountryNames, setFilteredCountryNames] = useState(
+    countryCodes.map((country) => country.name)
+  );
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      const keyPressed = e.key.toLowerCase();
+      if (/^[a-z]$/.test(keyPressed)) {
+        const filteredNames = countryCodes
+          .map((country) => country.name)
+          .filter((name) => name.toLowerCase().startsWith(keyPressed));
+        setFilteredCountryNames(filteredNames);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
-      <img className={styles.title} src={formImg} alt='img' />
+      <img className={styles.title} src={formImg} alt="img" />
       <form onSubmit={handleSubmit}>
         <div className={styles.inputContainer}>
           <label htmlFor="fullName">Full Name</label>
@@ -43,13 +70,13 @@ const Form = () => {
           />
         </div>
         <div className={styles.inputContainer}>
-          <label htmlFor="mobileNumber">Mobile Number</label>
-          <input
-            type="text"
-            id="mobileNumber"
-            value={mobileNumber}
-            onChange={(e) => setMobileNumber(e.target.value)}
-          />
+        <label htmlFor="requestServices">Mobile No</label>
+        <PhoneInput
+        className={styles.phonebox}
+        inputStyle={{border:"none", width: "100%"}}
+        buttonStyle={{border:"none"}}
+      placeholder="Enter phone number"
+     />
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor="requestServices">Request Services</label>
@@ -69,7 +96,9 @@ const Form = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <button type="submit" className={styles.submitBtn}>Submit</button>
+        <button type="submit" className={styles.submitBtn}>
+          Submit
+        </button>
       </form>
     </div>
   );
