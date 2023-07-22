69d57ee1b2e9c17c173d07b3df783d28376c027b
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-22 19:11:48 +0530

    form updated

---
+++ src/Component/ContactUsForm/ContactUsForm.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+import style from './ContactUsForm.module.css';
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+
+function ContactUsForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [interestedService, setInterestedService] = useState('');
+  const [projectBudget, setProjectBudget] = useState('');
+  const [projectType, setProjectType] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');
+  const [file, setFile] = useState(null);
+
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setFile(file);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = {
+      name,
+      email,
+      mobileNumber,
+      interestedService,
+      projectBudget,
+      projectType,
+      projectDescription,
+      file,
+    };
+    // Perform form validation and submit data to backend if needed
+    console.log(formData);
+  };
+
+  const handleScheduleMeeting = () => {
+    // Implement the logic for scheduling a meeting
+    console.log('Schedule Meeting clicked');
+  };
+
+  return (
+    <div className={style.main}>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <h6>How may we assist you today?</h6>
+        <div>
+          <label htmlFor="name">Your Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email Id:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="mobileNumber">Mobile Number:</label>
+          <PhoneInput
+             value={mobileNumber}
+             country={'in'}
+             onChange={(e) => setMobileNumber(e)}
+        className={style.phonebox}
+        inputStyle={{border:"none", width: "100%"}}
+        buttonStyle={{border:"none"}}
+      placeholder="Enter phone number"
+     />
+        </div>
+        <div>
+          <label htmlFor="interestedService">Interested Service:</label>
+          <select
+            id="interestedService"
+            name="interestedService"
+            value={interestedService}
+            onChange={(e) => setInterestedService(e.target.value)}
+          >
+            <option value="">Select an option</option>
+            <option value="service1">Service 1</option>
+            <option value="service2">Service 2</option>
+            {/* Add more options as needed */}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="projectBudget">Project Budget:</label>
+          <select
+            id="projectBudget"
+            name="projectBudget"
+            value={projectBudget}
+            onChange={(e) => setProjectBudget(e.target.value)}
+          >
+            <option value="">Select an option</option>
+            <option value="budget1">Budget 1</option>
+            <option value="budget2">Budget 2</option>
+            {/* Add more options as needed */}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="projectType">Project Type:</label>
+          <select
+            id="projectType"
+            name="projectType"
+            value={projectType}
+            onChange={(e) => setProjectType(e.target.value)}
+          >
+            <option value="">Select an option</option>
+            <option value="type1">Type 1</option>
+            <option value="type2">Type 2</option>
+            {/* Add more options as needed */}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="projectDescription">
+            Tell us more about your project:
+          </label>
+          <textarea
+            id="projectDescription"
+            name="projectDescription"
+            value={projectDescription}
+            onChange={(e) => setProjectDescription(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="file">Upload File:</label>
+          <input type="file" id="file" name="file" onChange={handleFileChange} />
+        </div>
+        <div>
+          <button type="button" onClick={handleScheduleMeeting}>
+            Schedule Meeting
+          </button>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default ContactUsForm;
--- src/Page/ContactUs/ContactUs.js
+++ src/Page/ContactUs/ContactUs.js
@@ -1,33 +1,10 @@
 import React, { useState } from "react";
 import style from "./ContactUs.module.css";
 import NavBar from "../../Component/NavBar/NavBar";
+import ContactUsForm from "../../Component/ContactUsForm/ContactUsForm";
 
 const ContactUs = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    mobileNumber: "",
-    interestedService: "",
-    projectBudget: "",
-    projectType: "",
-    projectDescription: "",
-  });
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Perform form validation and submit data to backend if needed
-    console.log(formData);
-  };
-
-  const handleScheduleMeeting = () => {
-    // Implement the logic for scheduling a meeting
-    console.log("Schedule Meeting clicked");
-  };
 
   return (
     <div className={style.main}>
@@ -39,102 +16,7 @@ const ContactUs = () => {
         sustainable, and equitable future Together.
       </p>
       <div className={style.container}>
-      <form className={style.form} onSubmit={handleSubmit}>
-        <h6>How may we assist you today?</h6>
-        <div>
-          <label htmlFor="name">Your Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email Id:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="mobileNumber">Mobile Number:</label>
-          <input
-            type="tel"
-            id="mobileNumber"
-            name="mobileNumber"
-            value={formData.mobileNumber}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="interestedService">Interested Service:</label>
-          <select
-            id="interestedService"
-            name="interestedService"
-            value={formData.interestedService}
-            onChange={handleInputChange}
-          >
-            <option value="">Select an option</option>
-            <option value="service1">Service 1</option>
-            <option value="service2">Service 2</option>
-            {/* Add more options as needed */}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="projectBudget">Project Budget:</label>
-          <select
-            id="projectBudget"
-            name="projectBudget"
-            value={formData.projectBudget}
-            onChange={handleInputChange}
-          >
-            <option value="">Select an option</option>
-            <option value="budget1">Budget 1</option>
-            <option value="budget2">Budget 2</option>
-            {/* Add more options as needed */}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="projectType">Project Type:</label>
-          <select
-            id="projectType"
-            name="projectType"
-            value={formData.projectType}
-            onChange={handleInputChange}
-          >
-            <option value="">Select an option</option>
-            <option value="type1">Type 1</option>
-            <option value="type2">Type 2</option>
-            {/* Add more options as needed */}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="projectDescription">
-            Tell us more about your project:
-          </label>
-          <textarea
-            id="projectDescription"
-            name="projectDescription"
-            value={formData.projectDescription}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="file">Upload File:</label>
-          <input type="file" id="file" name="file" />
-        </div>
-        <div>
-          <button type="button" onClick={handleScheduleMeeting}>
-            Schedule Meeting
-          </button>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+        <ContactUsForm/>
       </div>
    
     </div>
