ec64f2ae5314a6a8cd9d09808b2f0da8ee9b216e
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-07-28 20:03:17 +0530

    css

---
+++ src/Component/BlogForm/BlogForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+
+const BlogForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phoneNumber: '',
+    helpWith: '',
+    message: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here, you can handle the form submission, for example, sending the data to a server or processing it in some way.
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Your Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="helpWith">I need help with:</label>
+        <select
+          id="helpWith"
+          name="helpWith"
+          value={formData.helpWith}
+          onChange={handleInputChange}
+          required
+        >
+          <option value="">Select an option</option>
+          <option value="Technical Support">Technical Support</option>
+          <option value="Billing Inquiry">Billing Inquiry</option>
+          <option value="Product Information">Product Information</option>
+          <option value="Other">Other</option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleInputChange}
+          rows="5"
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default BlogForm;
--- src/Page/FullBlog2/FullBlog2.js
+++ src/Page/FullBlog2/FullBlog2.js
@@ -2,10 +2,47 @@ import React,{useState,useEffect} from 'react'
 import style from "./FullBlog2.module.css"
 import NavBar from '../../Component/NavBar/NavBar'
 import CallButton from '../../Component/CallButton/CallButton';
+import Footer from '../../Component/Footer/Footer';
+import BlogForm from '../../Component/BlogForm/BlogForm';
+import axios from 'axios';
 
 function FullBlog2() {
     const blog=JSON.parse(localStorage.getItem("blog2"))
     const [active, setActive] = useState(false);
+
+    const [cardsData, setCardsData] = useState([]);
+    const [search, setSearch] = useState("");
+    const [initialCardsData, setData] = useState([]);
+  
+  
+    useEffect(() => {
+      const handlegetData = async () => {
+        try {
+          const response = await axios.get('https://easyryt.onrender.com/admin/getAllBlog');
+          setData(response.data.data);
+          setCardsData([response.data.data[0]])
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      handlegetData();
+    }, []);
+  
+  
+  
+  
+  
+  
+    // console.log(cardsData,"ss")
+    localStorage.setItem("blog",JSON.stringify(cardsData))
+  
+    const handleCardClick = (heading) => {
+      const filteredCards = initialCardsData.filter(
+        (card) => card.heading === heading
+      );
+      setCardsData(filteredCards);
+    };
+  
 
     useEffect(() => {
       // Function to toggle the active state every 5 seconds
@@ -30,15 +67,48 @@ function FullBlog2() {
         <img    className={active ? style.active : ""}  src={blog[0]?.image}  alt='blog' />
         </div>
           <div className={style.infobox}>
-            <h2><span>Title:</span>{blog[0]?.title}</h2>
+            <div  className={style.info}>
+            <h3><span>Title:</span>{blog[0]?.title}</h3>
             <hr/>
-          <h3><span>Heading:</span>{blog[0]?.heading}</h3>
+          <h4><span>Heading:</span>{blog[0]?.heading}</h4>
           <hr/>
           <BlogContent htmlContent={blog[0]?.description} />
+            </div>
+          <div className={style.form}>
+          <BlogForm/>
+          <div className={style.rightBox}>
+          <input
+            className={style.searchInputbox}
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            placeholder="Search here..."
+          />
+          {initialCardsData
+            .filter((elem) => {
+              return elem.description
+                .toLowerCase()
+                .includes(search.toLowerCase());
+            })
+            .map((card, index) => (
+              <div
+                className={style.card}
+                key={index}
+                onClick={() => handleCardClick(card.heading)}
+              >
+                <div className={style.imgbox2}>
+                  <img src={card.blogImg} alt={card.heading} />
+                </div>
+                <h5>{card.headings}</h5>
+                <p className={style.parainfo} >{card.description}</p>
+              </div>
+            ))}
+        </div>
+          </div>
           </div>
     
       </div>
       <CallButton/>
+      <Footer/>
     </div>
   )
 }
