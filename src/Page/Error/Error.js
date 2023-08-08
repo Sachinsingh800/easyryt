b25363bf93ba746cee2360867298c725ef3d6171
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-08-08 14:10:24 +0530

    css

--- src/Page/Error/Error.js
+++ src/Page/Error/Error.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import style from "./Error.module.css"
 import img from "../../Image/Errors.gif"
-import { Link } from 'react-router-dom'
+import { Link, useParams } from "react-router-dom";
 
 function Error() {
+  const { blogTitle } = useParams(); 
+  console.log(blogTitle,"Full Blog")
+
   return (
     <div className={style.main}>
      <Link to={"/"}> <button className={style.btn}>Back to Home Page</button></Link>  
