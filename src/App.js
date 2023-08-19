a189aec7a378a88917d0f8188e3db07e2e54aaf7
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-08-19 11:11:01 +0530

    meta tag

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
         <title>We Transform Digital Solutions</title>
          <link rel='canonical' href='https://easyryt.com/' />
         <meta
-          name="Easyryt Software Solutions offers the tools you require to revolutionize & expedite your business. We stand as a prominent software development enterprise with over a decade of experience in crafting cutting-edge resolutions."
-          content="At Easyryt, we are dedicated to transforming your vision into reality through cutting-edge technology and innovative solutions. As a leading IT company, we offer a comprehensive range of services tailored to meet the unique requirements of businesses across diverse industries. With a talented team of experts, we take pride in delivering exceptional results that drive growth, efficiency, and success for our clients."
+          name="google-site-verification" 
+          content="bAYfY5U6tIPUgvlUMmjLrF4qJfpWPI3C9ZnkA7EYPLk"
         />
       </Helmet>
      <Home />
