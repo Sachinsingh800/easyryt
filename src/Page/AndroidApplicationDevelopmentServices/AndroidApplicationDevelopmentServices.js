c2977af654361db97480d15e9ff729ea0d4d7b44
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-08-24 15:04:50 +0530

    css

--- src/Page/AndroidApplicationDevelopmentServices/AndroidApplicationDevelopmentServices.js
+++ src/Page/AndroidApplicationDevelopmentServices/AndroidApplicationDevelopmentServices.js
@@ -13,7 +13,7 @@ import Section4 from "./Section4/Section4";
 import ForthSection from "../../Section/ForthSection/ForthSection";
 import ServiceForm from "../../Component/ServiceForm/ServiceForm";
 import Section5 from "./Section5/Section5";
-// import Section1 from "./section1/Section1";
+import Section1 from "./section1/Section1";
 
 function AndroidApplicationDevelopmentServices() {
   const [showPopup, setShowPopup] = useState(false);
@@ -49,7 +49,7 @@ function AndroidApplicationDevelopmentServices() {
           <img className={style.img} src={img} alt="img" />
         </div>
       </div>
-      {/* <Section1 /> */}
+      <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
