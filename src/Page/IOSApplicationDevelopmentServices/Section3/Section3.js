a52887240d16cfdd4e1e1ad5fb27d2e2e21e2578
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-08-24 15:50:55 +0530

    ios

--- src/Page/IOSApplicationDevelopmentServices/Section3/Section3.js
+++ src/Page/IOSApplicationDevelopmentServices/Section3/Section3.js
@@ -1,52 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import style from './Section3.module.css';
+import React from 'react'
+import style from "./Section3.module.css"
+import Tilt from "react-parallax-tilt";
+import { GiSkills } from 'react-icons/gi';
+import { GiCrafting } from 'react-icons/gi';
+import { MdPersonalInjury } from 'react-icons/md';
+import { SiClaris } from 'react-icons/si';
+import { TbTruckDelivery } from 'react-icons/tb';
+import { FaInnosoft } from 'react-icons/fa';
 
-const Section3 = () => {
-  const features = [
-    'Seamless Performance',
-    'Native User Interface',
-    'Offline Functionality',
-    'Enhanced Security',
-    'Access to Device Features',
-    'Better User Engagement',
-    'App Store Optimization',
-    'Faster Development',
-    'Long-Term Support',
-    'High-Quality Graphics',
-    'Security',
-  ];
+function Section3() {
 
-  const [currentFeatureIndex, setCurrentFeatureIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentFeatureIndex(getRandomIndex(features.length));
-    }, 2000); // 2-second delay
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const getRandomIndex = (length) => {
-    return Math.floor(Math.random() * length);
-  };
-
+  const data=[
+    {
+      icon:<GiSkills/>,
+      title:"Strategic Consultation:",
+      des:"Our initial step is to gain a deep understanding of your app's purpose, target audience, and distinguishing features. Our team of experts collaborates closely with you to define the objectives and scope of your project."
+    },
+    {
+      icon:<GiCrafting/>,
+      title:"Technology Selection:",
+      des:"The selection of the most appropriate technology stack is crucial for successful cross-platform development. Our developers, who have extensive experience in this field, choose the frameworks and tools that best suit the specific requirements of your project."
+    },
+    {
+      icon:<MdPersonalInjury/>,
+      title:"Design and User Experience:",
+      des:"Our talented design team creates interfaces that are intuitive and user-friendly, providing a consistent experience across different platforms. Our focus is on usability, aesthetics, and functionality to ensure maximum user engagement. "
+    },
+    {
+      icon:<SiClaris/>,
+      title:"Efficient Development:",
+      des:"The development process we follow adheres to the best practices in cross-platform development, resulting in a robust and scalable application. We utilize frameworks such as React Native, Flutter, and Xamarin to ensure optimal performance."
+    },
+    {
+      icon:<TbTruckDelivery/>,
+      title:"Testing and Quality Assurance:",
+      des:"Rigorous testing forms the core of our development process. We ensure that your app functions flawlessly on various devices and platforms, delivering a smooth and bug-free experience."
+    },
+    {
+      icon:<FaInnosoft/>,
+      title:"Deployment and Support:",
+      des:"We handle the entire deployment process across multiple app stores and provide ongoing support to address any issues that may arise and implement necessary updates."
+    },
+   
+  ]
   return (
     <div className={style.main}>
-      <h1>Features Of Native App Development Services</h1>
-      <p className={style.para}>
-        The Following Are Essential  Native App Development Services Featurees To Considen
-        When Developing a Successful Application For Your Company
-      </p>
-      <div className={style.innerbox}>
-        {features.map((feature, index) => (
-          <p key={index} className={index === currentFeatureIndex ? style.active : ''}>
-            ☑&#8193;{feature}
-          </p>
-        ))}
+      <h2>Why Select EasyRyt for Cross-platform  App Creation?</h2>
+      <div className={style.card_container} >
+      {data.map((item,id)=>
+
+   <Tilt glareEnable={true} tiltMaxAngleX={20} 
+   tiltMaxAngleY={20} perspective={1000} >
+       <div className={style.card}>
+        <h1>{item?.icon}</h1>
+          <h5 className={style.title}>{item?.title}</h5>
+          <p className={style.des}>{item?.des}</p>
+       </div>
+   </Tilt>
+
+      )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Section3;
-
+export default Section3
