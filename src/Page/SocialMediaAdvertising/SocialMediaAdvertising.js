03189b1ff3d06c7c0e44b1d63d0ec21af1a2cf89
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-08-26 13:26:48 +0530

    social ad page

--- src/Page/SocialMediaAdvertising/SocialMediaAdvertising.js
+++ src/Page/SocialMediaAdvertising/SocialMediaAdvertising.js
@@ -1,20 +1,24 @@
-import React , { useState } from "react";
+import React, { useState } from "react";
 import style from "./SocialMediaAdvertising.module.css";
 import NavBar from "../../Component/NavBar/NavBar";
 import img from "../../Image/social media marketing 1.png";
-import Section1 from "./Section1/Section1";
-import Section2 from "../FirstPage/Section2/Section2";
-import Section3 from "../FirstPage/Section3/Section3";
-import TenthSection from "../../Section/TenthSection/TenthSection";
+import Section2 from "./Section2/Section2";
+import Section3 from "./Section3/Section3";
 import SixthSection from "../../Section/SixthSection/SixthSection";
 import EleventhSection from "../../Section/EleventhSection/EleventhSection";
 import Footer from "../../Component/Footer/Footer";
 import CallButton from "../../Component/CallButton/CallButton";
 import PopupOption from "../../Component/PopupOption/PopupOption";
+import Section4 from "./Section4/Section4";
+import ForthSection from "../../Section/ForthSection/ForthSection";
+import ServiceForm from "../../Component/ServiceForm/ServiceForm";
+import Section5 from "./Section5/Section5";
+import Section from "./Section1/Section";
 
 function SocialMediaAdvertising() {
   const [showPopup, setShowPopup] = useState(false);
 
+  //popup
   const handleGetStartedClick = () => {
     setShowPopup(true);
   };
@@ -23,11 +27,17 @@ function SocialMediaAdvertising() {
       <NavBar />
       <div className={style.container}>
         <div>
-          <h1>Social Media Advertising Services</h1>
+          <h1>Social Media Advertising</h1>
+          <h6>Crafting Revolutionary Mobile Experiences</h6>
           <p>
-            Boost your brand's reach and engagement with our expert Social Media
-            Advertising Services. Targeted campaigns, compelling content, and
-            data-driven strategies for optimal results. 📱🚀 #SocialMediaAds
+            In the fast-paced digital world, cell programs have become a
+            critical medium for businesses to interact with their customers. A
+            crucial element of a hit cell app is its backend – the engine that
+            powers the app's capability, overall performance, and user
+            experience. The focus should be on crafting strong and efficient app
+            backends that lay the basis for high-quality app reports. Our App
+            Backend Development offerings are designed to empower your app with
+            seamless functionality, complete control, safety, and scalability.
           </p>
           <button
             type="submit"
@@ -37,24 +47,31 @@ function SocialMediaAdvertising() {
             Get Started
           </button>
         </div>
-        <div>
+        <div className={style.img_box}>
           <img className={style.img} src={img} alt="img" />
         </div>
       </div>
-      <Section1 />
+      <Section />
       <Section2 />
       <Section3 />
-      <TenthSection />
+      <Section4 />
+      <ServiceForm />
+      <ForthSection />
       <SixthSection />
       <EleventhSection />
-      <CallButton/>
+      <Section5 />
+      <CallButton />
       <Footer />
-            {/* Popup window */}
-            {showPopup && (
+      {showPopup && (
         <>
-        <button onClick={()=>setShowPopup(false)} className={style.closebtn}>Close</button>
-      <PopupOption/>
-      </>
+          <button
+            onClick={() => setShowPopup(false)}
+            className={style.closebtn}
+          >
+            Close
+          </button>
+          <PopupOption />
+        </>
       )}
     </div>
   );
