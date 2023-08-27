41bbada0cc498af466c7b7ed0f7ff895ba062981
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-08-28 00:47:00 +0530

    css

--- src/Page/IOSAppAdvertising/IOSAppAdvertising.js
+++ src/Page/IOSAppAdvertising/IOSAppAdvertising.js
@@ -1,20 +1,24 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import style from "./IOSAppAdvertising.module.css";
 import NavBar from "../../Component/NavBar/NavBar";
 import img from "../../Image/IOS App Advertising 1.png";
-import Section1 from "./Section1/Section1";
-import Section2 from "../FirstPage/Section2/Section2";
-import Section3 from "../FirstPage/Section3/Section3";
-import TenthSection from "../../Section/TenthSection/TenthSection";
+import Section2 from "./Section2/Section2";
+import Section3 from "./Section3/Section3";
 import SixthSection from "../../Section/SixthSection/SixthSection";
 import EleventhSection from "../../Section/EleventhSection/EleventhSection";
 import Footer from "../../Component/Footer/Footer";
 import CallButton from "../../Component/CallButton/CallButton";
 import PopupOption from "../../Component/PopupOption/PopupOption";
+import Section4 from "./Section4/Section4";
+import ForthSection from "../../Section/ForthSection/ForthSection";
+import ServiceForm from "../../Component/ServiceForm/ServiceForm";
+import Section5 from "./Section5/Section5";
+import Section from "./Section1/Section";
 
 function IOSAppAdvertising() {
   const [showPopup, setShowPopup] = useState(false);
 
+  //popup
   const handleGetStartedClick = () => {
     setShowPopup(true);
   };
@@ -23,11 +27,17 @@ function IOSAppAdvertising() {
       <NavBar />
       <div className={style.container}>
         <div>
-          <h1>IOS App Advertising Services</h1>
+          <h1>App Backend Development</h1>
+          <h6>Crafting Revolutionary Mobile Experiences</h6>
           <p>
-            Boost your app's success with our iOS App Advertising Services.
-            Targeted campaigns, optimal visibility, and increased downloads.
-            Maximize your app's potential today!
+            In the fast-paced digital world, cell programs have become a
+            critical medium for businesses to interact with their customers. A
+            crucial element of a hit cell app is its backend – the engine that
+            powers the app's capability, overall performance, and user
+            experience. The focus should be on crafting strong and efficient app
+            backends that lay the basis for high-quality app reports. Our App
+            Backend Development offerings are designed to empower your app with
+            seamless functionality, complete control, safety, and scalability.
           </p>
           <button
             type="submit"
@@ -41,20 +51,27 @@ function IOSAppAdvertising() {
           <img className={style.img} src={img} alt="img" />
         </div>
       </div>
-      <Section1 />
+      <Section />
       <Section2 />
       <Section3 />
-      <TenthSection />
+      <Section4 />
+      <ServiceForm />
+      <ForthSection />
       <SixthSection />
       <EleventhSection />
-      <CallButton/>
+      <Section5 />
+      <CallButton />
       <Footer />
-            {/* Popup window */}
-            {showPopup && (
+      {showPopup && (
         <>
-        <button onClick={()=>setShowPopup(false)} className={style.closebtn}>Close</button>
-      <PopupOption/>
-      </>
+          <button
+            onClick={() => setShowPopup(false)}
+            className={style.closebtn}
+          >
+            Close
+          </button>
+          <PopupOption />
+        </>
       )}
     </div>
   );
