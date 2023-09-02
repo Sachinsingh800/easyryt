a99405cd3995d323f60c567eec135cd92b1a0d68
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-09-02 23:14:22 +0530

    css

--- src/Page/AndroidApplicationDevelopmentServices/AndroidApplicationDevelopmentServices.js
+++ src/Page/AndroidApplicationDevelopmentServices/AndroidApplicationDevelopmentServices.js
@@ -20,6 +20,37 @@ import { Helmet } from "react-helmet";
 function AndroidApplicationDevelopmentServices() {
   const [showPopup, setShowPopup] = useState(false);
 
+
+  // JSON-LD script for Android Application Development Services
+const jsonLdScript = {
+  "@context": "https://schema.org/",
+  "@type": "Product",
+  "name": "Android Application Development Services",
+  "image": "https://www.easyryt.com/static/media/Android%20Application%20Development%20Services%201.b68f17f9ca4d1fc1c314.png",
+  "description": "Android, as one of the widely used operating systems globally, provides an expansive platform to reach a diverse target audience. Our Android app development services are designed to help organizations harness the power of this platform and create modern, user-friendly, and high-performance applications that drive results.",
+  "brand": {
+    "@type": "Brand",
+    "name": "EasyRyt"
+  },
+  "aggregateRating": {
+    "@type": "AggregateRating",
+    "ratingValue": "5",
+    "ratingCount": "1",
+    "reviewCount": "1"
+  },
+  "review": {
+    "@type": "Review",
+    "name": "Top-Notch Android App Development Services",
+    "reviewBody": "I recently worked with EasyRyt's exceptional team for Android app development. Their technical expertise, user-centric approach, and commitment to deadlines were outstanding. I highly recommend their services for your next Android app project.",
+    "reviewRating": {
+      "@type": "Rating",
+      "ratingValue": "5"
+    },
+    "author": { "@type": "Person", "name": "Yaman Ali" }
+  }
+};
+
+
   const handleGetStartedClick = () => {
     setShowPopup(true);
   };
@@ -32,6 +63,10 @@ function AndroidApplicationDevelopmentServices() {
     name="Expert Android App Development"
     content="Turn your vision into reality with our top-tier Android app development services. Boost engagement and growth."
   />
+        {/* Include the JSON-LD script within the head */}
+        <script type="application/ld+json">
+          {JSON.stringify(jsonLdScript)}
+        </script>
 </Helmet>
       <NavBar />
       <div className={style.container}>
