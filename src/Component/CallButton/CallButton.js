acd0c961e8afc0f981f0c2a0aa8446b4b0c042fd
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-09-14 14:32:23 +0530

    no change

--- src/Component/CallButton/CallButton.js
+++ src/Component/CallButton/CallButton.js
@@ -6,7 +6,7 @@ import whatsapp from "../../Image/whatsapp.png"
 function CallButton() {
     const handleWhatsAppClick = () => {
         // Replace the following phone number with the desired WhatsApp number
-        const phoneNumber = '8287815883';
+        const phoneNumber = '7678109682';
         const message = 'Hello! I have a question.';
         const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
         window.open(url, '_blank');
@@ -14,7 +14,7 @@ function CallButton() {
     
       const handleCallClick = () => {
         // Replace the following phone number with the desired phone number
-        const phoneNumber = '8287815883';
+        const phoneNumber = '7678109682';
         const url = `tel:${phoneNumber}`;
         window.location.href = url;
       };
